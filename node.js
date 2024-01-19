4d496b9762ed7c36a057537fd3d792c4b3372810
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2024-01-19 19:29:14 -0300

    primeiro commit

---
+++ node.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        "tabWidth": 4,
+        "useTabs": true,
+        "semi": false,
+        "arrowParens": "avoid",
+        "printWidth": 130,
+        "singleQuote": true,
+        "trailingComma": "all",
+        "endOfLine": "auto"
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+}
